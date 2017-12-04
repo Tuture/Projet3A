b157d6ac398bdf049cf61dbc2be4b3bdfbfad1eb
Fatima <Fatima@Fatima.local>
2017-12-04 14:37:15 +0100

    Initial Commit

---
+++ Projet3A/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projet3A
+//
+//  Created by Fatima on 04/12/2017.
+//  Copyright © 2017 Fatima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
