f8fba6adb43bc74f619d412472ee9fa445402311
Cours iPhone <coursiphone@iMac-cours-6.local>
2017-12-05 18:56:34 +0100

    contact list and contact add

---
+++ Projet3A/AddPointViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddPointViewController.swift
+//  Projet3A
+//
+//  Created by Cours iPhone on 05/12/2017.
+//  Copyright © 2017 Fatima. All rights reserved.
+//
+
+import UIKit
+
+class AddPointViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Projet3A/Point.swift
@@ -0,0 +1,16 @@
+//
+//  Point.swift
+//  Projet3A
+//
+//  Created by Cours iPhone on 05/12/2017.
+//  Copyright © 2017 Fatima. All rights reserved.
+//
+
+import Foundation
+
+struct Point {
+    let nom : String
+    let adresse : String
+    let lat : Double
+    let long : Double
+}
